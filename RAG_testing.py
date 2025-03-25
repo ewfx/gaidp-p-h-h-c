6099065e3576ca1cbcf1d836bc615a1e8b521791
Roshan Prashant Bara <roshanprashantbara@gmail.com>
2025-03-26 01:17:18 +0530

    Final working code

--- RAG_testing.py
+++ RAG_testing.py
@@ -8,66 +8,104 @@ from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 import sys
 from configuration.config import CONFIG
+import time
 
 # Force UTF-8 for printing
 sys.stdout.reconfigure(encoding='utf-8')
 
-def save_chat_history(user_input, response):
-    with open(CONFIG['CHAT_HISTORY_PATH'], 'a', encoding='utf-8') as f:
-        f.write(f"User: {user_input}\nAI: {response}\n\n")
+def save_generated_rules(response):
+    with open(CONFIG['GENERATED_RULES_PATH'], 'a', encoding='utf-8') as f:
+        f.write(response)
 
-def load_chat_history():
-    if os.path.exists(CONFIG['CHAT_HISTORY_PATH']):
-        with open(CONFIG['CHAT_HISTORY_PATH'], 'r', encoding='utf-8') as f:
+def save_generated_sql( response):
+    with open(CONFIG['GENERATED_SQL_PATH'], 'a', encoding='utf-8') as f:
+        f.write(response)
+
+def load_generated_rules():
+    if os.path.exists(CONFIG['GENERATED_RULES_PATH']):
+        with open(CONFIG['GENERATED_RULES_PATH'], 'r', encoding='utf-8') as f:
             return f.read()
     return ""
 
 load_dotenv()
-
-# Define the directory containing the PDF file and the persistent directory
-file_path = "documents/finra.pdf"
-output_path = "documents/extracted_text.txt"
-current_dir = os.path.dirname(os.path.abspath(__file__))
-persistent_directory = os.path.join(current_dir, "db", "chroma_db")
-
-pdf = PdfReader(file_path)
-# Generate the required page numbers
-pages_to_load = list(range(165,170))
-
-# Ensure the PDF file exists
-if not os.path.exists(file_path):
-    raise FileNotFoundError(
-        f"The file {file_path} does not exist. Please check the path."
-    )
-
-    # Read text from PDF
-raw_text = ""
-for page_num in pages_to_load:
-    content = pdf.pages[page_num].extract_text()
-    if content:
-        # Fix broken words by removing line breaks followed by non-space characters
-        raw_text += f"\n--- Page {page_num + 1} ---\n{content}"
-
-        # embeddings_model = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-        # embeddings = embeddings_model.embed_documents(output_path)
-
-query = "Generate data profiling rules for Corporate Loan Data Schedule based on fields provided as per regulatory standard considering the rule description and allowable values provided in the document Generate RULES that can be used to flag transactions, just return a json containing two field in first field is rule and second is the coresponding sql query without explanation "
-
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-combined_input = (
-    "Here are some documents that might help answer the question: "
-    + query
-    + "\n\nRelevant Documents:\n"
-    + "\n\n".join([raw_text])
-)
-# Define the messages for the model
-messages = [
-    SystemMessage(content="You are a financial risk expert working in a bank."),
-    HumanMessage(content=combined_input),
-]
-result = llm.invoke(messages)
-save_chat_history(query, result.content)
+# llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
+def generate_sql_from_rules():
+    generate_rules = load_generated_rules()
+    query = "Generate SQL queries for the given rules in json format containing two fields, the rule and the corresponding SQL query. There is only one table with field names provided in the rules."
+    # query = "Generate executable SQL queries for the given rules in JSON format with two fields: rule, containing a clear description of the rule, and query, containing the corresponding SQL query. Assume all fields referenced in the rules belong to a single table, and use the exact field names provided. Ensure the SQL queries include standard SQL syntax for filtering, validation, and flagging transactions. Handle data type checks, range validations, format compliance, and missing/null value checks. Use logical operators such as AND, OR, and functions like IS NULL, LIKE, and BETWEEN appropriately. Write the queries in a consistent format for readability and execution, and return the full record when the condition is met using SELECT * or the relevant fields. The output should be a JSON array, with each object containing the rule and the corresponding query. If data point is not availaible don not post some random value, fill it with some relevant values"
+
+    combined_input = (
+        "Here are some documents that might help answer the question: "
+        + query
+        + f"\n\nGiven Rules:\n{generate_rules}"
+    )
+    # Define the messages for the model
+    messages = [
+        SystemMessage(content="You are a SQL expert."),
+        HumanMessage(content=combined_input),
+    ]
+    result = llm.invoke(messages)
+    save_generated_sql(result.content)
+
+
+def generate_rules():
+    # Define the directory containing the PDF file and the persistent directory
+    file_path = CONFIG["COMPLIANCE_DOC_FILE_PATH"]
+    # output_path = "documents/extracted_text.txt"
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+
+    pdf = PdfReader(file_path)
+    # Generate the required page numbers
+    PAGE_INTERVAL = 5
+    PAGE_START = 166
+    PAGE_END = 181
+    pages_to_load = list(range(PAGE_START, PAGE_END, PAGE_INTERVAL))
+    print(pages_to_load)
+
+    # Ensure the PDF file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"The file {file_path} does not exist. Please check the path."
+        )
+
+        # Read text from PDF
+    for start_page_num in pages_to_load:
+        raw_text = ""
+        for page_num in range(start_page_num, min(start_page_num + PAGE_INTERVAL, PAGE_END)):
+            content = pdf.pages[page_num].extract_text()
+            if content:
+                # Fix broken words by removing line breaks followed by non-space characters
+                raw_text += f"\n{content}"
+
+                # embeddings_model = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+                # embeddings = embeddings_model.embed_documents(output_path)
+        # print(raw_text)
+        # chat_history = ""
+        # query = "Generate data profiling rules for the Corporate Loan Data Schedule based on the fields, rule descriptions, and allowable values provided in the document. Create specific rules to flag transactions that violate data integrity, consistency, completeness, and accuracy standards. Ensure the rules account for data types (e.g., numeric, string, date validation), value ranges and limits (e.g., min/max amounts, valid date ranges), format validation (e.g., valid currency codes, date formats), null or missing values, and referential integrity (e.g., valid relationships between fields). Use clear and concise rule syntax that can be easily interpreted by an LLM or used in a profiling tool. Do not include explanations—only return the profiling rules. Provide the rules in json format."
+        query = "Generate data profiling rules for Corporate Loan Data Schedule based on fields provided considering the rule description and allowable values provided in the document Generate RULES that can be used to flag transactions, just return a json containing two field in first field is rule and second is the coresponding sql query without explanation rovide the rules in json format"
+
+        combined_input = (
+            "Here are some documents that might help answer the question: "
+            + query
+            + "\n\nRelevant Documents:\n"
+            + "\n\n".join([raw_text])
+            # + f"\n\nChat History:\n{chat_history}"
+        )
+        # Define the messages for the model
+        messages = [
+            SystemMessage(content="You are a financial risk expert working in a bank."),
+            HumanMessage(content=combined_input),
+        ]
+        result = llm.invoke(messages)
+        save_generated_rules(result.content)
+        time.sleep(1.5)
+        print(f"Request sent: {start_page_num} to {min(start_page_num + PAGE_INTERVAL, PAGE_END)}")
+
+# generate_rules()
+generate_sql_from_rules()
 # Check if the Chroma vector store already exists
 
 
--- app1.py
+++ app1.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import os
 from configuration.config import CONFIG
-from model import model
-from extractSQL import extractSQL, flagTransactions
+from extractRulesSQL import extract_rules_sql, flagTransactions
 
 
 # Specify the path to your logo image
@@ -22,6 +21,9 @@ if "page" not in st.session_state:
 if "form_submitted" not in st.session_state:
     st.session_state.form_submitted = False
 
+files_upload_flag = False
+generate_rules_flag = False
+evaluate_data_flag = False
 
 # Navigation function
 def next_page(page_name):
@@ -35,16 +37,13 @@ def getRules():
 
 if st.session_state.page == "upload":
     # Create a container
-    uploadContainer = st.container()
+    uploadContainer = st.container(border=True)
     uploadContainer.header("Upload Compliance Instructions and Reporting Data")
 
     # Form to upload files
-    with uploadContainer.form(key="uploadForm"):
-        instrucDoc = st.file_uploader("Upload your Compliance instructions document", type=["pdf"])
-        reportingDataFile = st.file_uploader("Upload the reporting data", type=["csv"])
-        
-        # Submit button
-        submit_button = st.form_submit_button("Upload", use_container_width=True)
+    instrucDoc = uploadContainer.file_uploader("Upload your Compliance instructions document", type=["pdf"])
+    reportingDataFile = uploadContainer.file_uploader("Upload the reporting data", type=["csv"])
+    submit_button = uploadContainer.button("Upload", use_container_width=True)
 
     # Initialize dictionary to store file names
     file_names = {}
@@ -55,11 +54,8 @@ if st.session_state.page == "upload":
             st.error("Please upload both files before submitting.")
         else:
             # Create the directory if it doesn't exist
-            save_dir = CONFIG["DATA_DIR"]
-            os.makedirs(save_dir, exist_ok=True)
+            os.makedirs(CONFIG["DATA_DIR"], exist_ok=True)
 
-            # Save the files
-            # instruc_path = os.path.join(save_dir, instrucDoc.name)
             instruc_path = CONFIG["COMPLIANCE_DOC_FILE_PATH"]
             reporting_path = CONFIG["DATA_FILE_PATH"]
 
@@ -69,53 +65,51 @@ if st.session_state.page == "upload":
             with open(reporting_path, "wb") as f:
                 f.write(reportingDataFile.getbuffer())
 
-            # Display success message and file details
             st.success("Files uploaded successfully!")
-
+            st.session_state.form_submitted = True
 
             # Immediately move to the next page after successful form submission
-            st.session_state.form_submitted = True
             if st.session_state.form_submitted:
                 next_page("generate")
-                st.rerun()  # Force the app to refresh immediately
-            print("Hello")
+            generateButton = uploadContainer.button("Generate Rules", type="primary", use_container_width=True)
 
 elif st.session_state.page == "generate":
     # Create a container for generating rules
     generateContainer = st.container()    
-    generateContainer.subheader("Refine Rules")
+    generateContainer.subheader("Generated Rules form the Compliance Instructions")
     rulesWindow = generateContainer.container(height=500)
     # rawRules = model()
     # rulesList = rawRules.split("\n")
-    rulesList = ['1.  Loan Amount > $10,000,000 and Internal Risk Rating in bottom two risk grades.', '2.  Past Due > 90 days and Outstanding Balance Fair Value < 50% of Original Loan Amount.', '3.  Guarantor LEI missing and Loan Amount > $5,000,000.', '4.  Purpose of Loan = "Unclassified" and Loan Amount > $1,000,000.', '5.  Country of Incorporation = High-Risk Jurisdiction and Loan Amount > $2,500,000.', '6.  Significant increase in Outstanding Balance Fair Value in a short period.', '7.  NAICS Code in a high-risk industry and Loan Amount > $5,000,000.', "8.  Borrower's Total Assets < $10,000,000 and Loan Amount > $1,000,000.", "9.  Borrower's Total Liabilities / Total Assets > 0.8 and Loan Amount > $2,500,000.", "10. Borrower's Net Income < 0 for the last two reporting periods and Loan Amount > $1,000,000.", '11. Loan Original Maturity Date within the next 12 months and no refinancing plan.', '12.  Collateral Type = "Intangible Assets" and Loan Amount > $5,000,000.', '13.  Loan Syndicated Flag = "Yes" and Lead Lender LEI missing.', '14.  Number of Lenders > 10 and Loan Amount > $25,000,000.', '15.  Loan Purpose = "Leveraged Buyout" and Loan Amount > $10,000,000.', '16.  Loan Type = "Commercial Real Estate" and LTV Ratio > 0.9.', "17.  Borrower's Debt Service Coverage Ratio < 1.0 and Loan Amount > $2,500,000.", "18.  Significant change in Borrower's NAICS Code.", '19.  Missing or invalid Obligor LEI for Loan Amount > $1,000,000.', '20.  Line of Business = "Unclassified" and Loan Amount > $1,000,000.', '21.  PD Calculation Method = "Point in Time" and Maximum Probability of Default > 0.1.', '22.  Internal Risk Rating changed significantly in a short period.', '23.  Loan Secured Flag = "No" and Loan Amount > $5,000,000.', "24.  Guarantor's Total Assets < $5,000,000 and Loan Amount > $1,000,000.", '25.  Missing or invalid Guarantor LEI for Loan Amount > $1,000,000.', '26.  Significant change in Outstanding Balance Amount in a short period.', '27.  Outstanding Balance Amount = 0 and Loan Status = "Active".', '28.  Loan Original Maturity Date in the past.', '29.  Combination of high LTV ratio and low Debt Service Coverage Ratio.', '30.  Multiple loans to the same obligor with aggregate exposure exceeding a defined threshold.']
-    # print(f"-----------------------------------{type(rawRules)}")
-    print(rulesList)
-    # for rule in rulesList:
-    #     rulesWindow.checkbox(rule, key=rule)
-    rulesWindow.markdown("\n".join(rulesList))
-    evaluateButton = generateContainer.button("Evaluate Data", type="primary", use_container_width=True)
-    cancelButton = generateContainer.button("Cancel", use_container_width=True)
-    if(cancelButton):
-        next_page("upload")
-        st.rerun()
+    # rulesList = ['1.  Loan Amount > $10,000,000 and Internal Risk Rating in bottom two risk grades.', '2.  Past Due > 90 days and Outstanding Balance Fair Value < 50% of Original Loan Amount.', '3.  Guarantor LEI missing and Loan Amount > $5,000,000.', '4.  Purpose of Loan = "Unclassified" and Loan Amount > $1,000,000.', '5.  Country of Incorporation = High-Risk Jurisdiction and Loan Amount > $2,500,000.', '6.  Significant increase in Outstanding Balance Fair Value in a short period.', '7.  NAICS Code in a high-risk industry and Loan Amount > $5,000,000.', "8.  Borrower's Total Assets < $10,000,000 and Loan Amount > $1,000,000.", "9.  Borrower's Total Liabilities / Total Assets > 0.8 and Loan Amount > $2,500,000.", "10. Borrower's Net Income < 0 for the last two reporting periods and Loan Amount > $1,000,000.", '11. Loan Original Maturity Date within the next 12 months and no refinancing plan.', '12.  Collateral Type = "Intangible Assets" and Loan Amount > $5,000,000.', '13.  Loan Syndicated Flag = "Yes" and Lead Lender LEI missing.', '14.  Number of Lenders > 10 and Loan Amount > $25,000,000.', '15.  Loan Purpose = "Leveraged Buyout" and Loan Amount > $10,000,000.', '16.  Loan Type = "Commercial Real Estate" and LTV Ratio > 0.9.', "17.  Borrower's Debt Service Coverage Ratio < 1.0 and Loan Amount > $2,500,000.", "18.  Significant change in Borrower's NAICS Code.", '19.  Missing or invalid Obligor LEI for Loan Amount > $1,000,000.', '20.  Line of Business = "Unclassified" and Loan Amount > $1,000,000.', '21.  PD Calculation Method = "Point in Time" and Maximum Probability of Default > 0.1.', '22.  Internal Risk Rating changed significantly in a short period.', '23.  Loan Secured Flag = "No" and Loan Amount > $5,000,000.', "24.  Guarantor's Total Assets < $5,000,000 and Loan Amount > $1,000,000.", '25.  Missing or invalid Guarantor LEI for Loan Amount > $1,000,000.', '26.  Significant change in Outstanding Balance Amount in a short period.', '27.  Outstanding Balance Amount = 0 and Loan Status = "Active".', '28.  Loan Original Maturity Date in the past.', '29.  Combination of high LTV ratio and low Debt Service Coverage Ratio.', '30.  Multiple loans to the same obligor with aggregate exposure exceeding a defined threshold.']
+
+    rulesSQLList = extract_rules_sql()
+
+    for item in rulesSQLList:
+        rule_container = rulesWindow.container(border=True)
+        rule_container.markdown(item["rule"])
+        # rule_container.divider()
+    evaluateButton = generateContainer.button("Generate formulas from rules", type = "secondary", use_container_width=True)
     if(evaluateButton):
         next_page("evaluate")
+        generateContainer.button("Evaluate Data", type="primary", use_container_width=True)
+        # st.rerun()
+    cancelButton = generateContainer.button("Cancel", type = "tertiary", use_container_width=True)
+    if(cancelButton):
+        next_page("upload")
         st.rerun()
 elif st.session_state.page == "evaluate":
     # Create a container for generating rules
     evaluateContainer = st.container()
     evaluateContainer.subheader("Evaluate Data")
     evaluateContainer.write("Flagged transactions due to rules are as following:")
-    flaggedTr = flagTransactions()
+    rulesSQLList = extract_rules_sql()
+    flaggedTr, rules_linked = flagTransactions(rulesSQLList)
+    print("Hello")
     for i, tr in enumerate(flaggedTr):
-        with st.expander(f"Rule {i+1}"):
+        with st.expander(f"Rule {i+1}: {rules_linked[i]}"):
             st.dataframe(tr)
 
     homeButton = st.button("Home", use_container_width=True)
     if(homeButton):
         next_page("upload")
         st.rerun()
-    # next_page("upload")
-    # st.rerun()
-
-# Create a container for generating rules
